e7973a6da8d1fa645c8ffd424d3fa444f254d0e8
ioexcept <ioexcept@yahoo.com>
2024-04-18 11:54:26 -0500

    added java structure

---
+++ src/main/java/app/iapetus/ui/DataManager.java
@@ -0,0 +1,14 @@
+package app.iapetus.ui;
+
+public class DataManager {
+
+	public DataManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Saturn's moon Iapetus!");
+	}	
+
+}
